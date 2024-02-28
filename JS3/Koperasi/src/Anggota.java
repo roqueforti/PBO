74f5700550595a8a08a2812b5822c199ec950df1
roqueforti <budiarto3788@gmail.com>
2024-02-28 09:52:14 +0700

    jobsheet 3

---
+++ JS3/Koperasi/src/Anggota.java
@@ -0,0 +1,42 @@
+public class App {
+    private String nama;
+    private int nomorKtp;
+    private int limitPeminjaman;
+    private int jumlahPinjaman;
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNomorKtp(int nomorKtp) {
+        this.nomorKtp = nomorKtp;
+    }
+
+    public void setLimitPeminjaman(int limitPeminjaman) {
+        this.limitPeminjaman = limitPeminjaman;
+    }
+
+    public void setJumlahPinjaman(int jumlahPinjaman) {
+        this.jumlahPinjaman = jumlahPinjaman;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getNomorKtp() {
+        return nomorKtp;
+    }
+
+    public int getLimitPeminjaman() {
+        return limitPeminjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(int pinjam){
+        
+    }
+}
